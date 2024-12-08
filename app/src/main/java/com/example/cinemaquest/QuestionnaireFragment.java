1e72bcd09dba4457c370344a97e8d894ec830bc0
James Davis <jamesmd1996@yahoo.com>
2024-12-08 18:57:40 -0500

    update questionnaire

--- app/src/main/java/com/example/cinemaquest/QuestionnaireFragment.java
+++ app/src/main/java/com/example/cinemaquest/QuestionnaireFragment.java
@@ -5,6 +5,7 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,32 +28,22 @@ public class QuestionnaireFragment extends Fragment {
     private DatabaseHelper dbHelper; // Declare dbHelper
 
     private String[] questions = {
-            "What is your favorite movie genre?",
-            "Which movie do you prefer?",
-            "What kind of movie ending do you like?",
-            "Which actor do you prefer?",
-            "What type of movie soundtrack do you enjoy?",
-            "Which movie setting do you prefer?",
-            "What kind of movie plot do you enjoy?",
-            "Which movie director do you prefer?",
-            "What movie length do you prefer?",
-            "Which movie era do you prefer?"
+            "What genre do you prefer?",
+            "What type of movie setting do you enjoy?",
+            "What is your favorite movie era?",
+            "What mood are you in?",
+            "What kind of storyline do you prefer?"
     };
 
     private String[][] answers = {
             {"Action", "Comedy", "Drama", "Sci-Fi"},
-            {"Inception", "Titanic", "The Godfather", "Star Wars"},
-            {"Happy", "Sad", "Cliffhanger", "Open-ended"},
-            {"Leonardo DiCaprio", "Tom Hanks", "Meryl Streep", "Scarlett Johansson"},
-            {"Classical", "Rock", "Pop", "Instrumental"},
-            {"Urban", "Rural", "Space", "Fantasy"},
-            {"Linear", "Non-linear", "Multiple plots", "Single plot"},
-            {"Steven Spielberg", "Christopher Nolan", "Quentin Tarantino", "Martin Scorsese"},
-            {"Short", "Medium", "Long", "Epic"},
-            {"Classic", "Modern", "Future", "Historical"}
+            {"Urban", "Rural", "Fantasy", "Historical"},
+            {"1990s", "2000", "2010s", "2020s"},
+            {"Happy", "Sad", "Excited", "Relaxed"},
+            {"Adventure", "Romance", "Mystery", "Thriller"}
     };
 
-    private TextView[] indicators = new TextView[10];
+    private TextView[] indicators = new TextView[5];
     private Button finishButton;
     private HashMap<Integer, String> selectedAnswers = new HashMap<>();
 
@@ -66,18 +57,22 @@ public class QuestionnaireFragment extends Fragment {
         finishButton = view.findViewById(R.id.finish_button);
         dbHelper = new DatabaseHelper(getContext());
 
+        // Clear previous answers from the database
+        clearPreviousAnswers();
+
         // Initialize indicators
         indicators[0] = view.findViewById(R.id.indicator_1);
         indicators[1] = view.findViewById(R.id.indicator_2);
         indicators[2] = view.findViewById(R.id.indicator_3);
         indicators[3] = view.findViewById(R.id.indicator_4);
         indicators[4] = view.findViewById(R.id.indicator_5);
-        indicators[5] = view.findViewById(R.id.indicator_6);
-        indicators[6] = view.findViewById(R.id.indicator_7);
-        indicators[7] = view.findViewById(R.id.indicator_8);
-        indicators[8] = view.findViewById(R.id.indicator_9);
-        indicators[9] = view.findViewById(R.id.indicator_10);
 
+        // Check for null indicators and log an error if any are null
+        for (int i = 0; i < indicators.length; i++) {
+            if (indicators[i] == null) {
+                Log.e("QuestionnaireFragment", "Indicator " + (i + 1) + " is null");
+            }
+        }
         // Set up listeners and initial state
         updateQuestion();
 
@@ -110,8 +105,14 @@ public class QuestionnaireFragment extends Fragment {
         return view;
     }
 
+    private void clearPreviousAnswers() {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        db.delete("answers", null, null);
+        db.close();
+    }
+
     private void updateQuestion() {
-        // Update question logic
+        // Update question text and answer options
         questionText.setText(questions[currentQuestion - 1]);
         answerOptions.removeAllViews();
         for (String answer : answers[currentQuestion - 1]) {
@@ -120,22 +121,10 @@ public class QuestionnaireFragment extends Fragment {
             answerOptions.addView(radioButton);
         }
 
-        // Restore previously selected answer if available
-        if (selectedAnswers.containsKey(currentQuestion)) {
-            String selectedAnswer = selectedAnswers.get(currentQuestion);
-            for (int i = 0; i < answerOptions.getChildCount(); i++) {
-                RadioButton radioButton = (RadioButton) answerOptions.getChildAt(i);
-                if (radioButton.getText().equals(selectedAnswer)) {
-                    radioButton.setChecked(true);
-                    break;
-                }
-            }
-        }
-
-        // Update question indicators
+        // Update indicators
         updateIndicators();
 
-        // Enable or disable the navigation buttons based on the question position
+        // Enable/disable navigation buttons
         previousButton.setEnabled(currentQuestion > 1);
         nextButton.setEnabled(currentQuestion < questions.length);
 
@@ -151,10 +140,12 @@ public class QuestionnaireFragment extends Fragment {
 
     private void updateIndicators() {
         for (int i = 0; i < indicators.length; i++) {
-            if (i == currentQuestion - 1) {
+            if (i < currentQuestion - 1) {
+                indicators[i].setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
+            } else if (i == currentQuestion - 1) {
                 indicators[i].setBackgroundColor(getResources().getColor(android.R.color.holo_blue_light));
             } else {
-                indicators[i].setBackgroundColor(getResources().getColor(android.R.color.transparent));
+                indicators[i].setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
             }
         }
     }
@@ -163,8 +154,7 @@ public class QuestionnaireFragment extends Fragment {
         int selectedId = answerOptions.getCheckedRadioButtonId();
         if (selectedId != -1) {
             RadioButton selectedRadioButton = answerOptions.findViewById(selectedId);
-            String selectedAnswer = selectedRadioButton.getText().toString();
-            selectedAnswers.put(currentQuestion, selectedAnswer);
+            selectedAnswers.put(currentQuestion, selectedRadioButton.getText().toString());
         }
     }
 
@@ -173,16 +163,9 @@ public class QuestionnaireFragment extends Fragment {
         ContentValues values = new ContentValues();
 
         for (int i = 1; i <= questions.length; i++) {
-            if (selectedAnswers.containsKey(i)) {
-                values.put(DatabaseHelper.COLUMN_QUESTION, questions[i - 1]);
-                values.put(DatabaseHelper.COLUMN_ANSWER, selectedAnswers.get(i));
-                String whereClause = DatabaseHelper.COLUMN_QUESTION + " = ?";
-                String[] whereArgs = {questions[i - 1]};
-                int rowsUpdated = db.update(DatabaseHelper.TABLE_ANSWERS, values, whereClause, whereArgs);
-                if (rowsUpdated == 0) {
-                    db.insert(DatabaseHelper.TABLE_ANSWERS, null, values);
-                }
-            }
+            values.put("question", questions[i - 1]);
+            values.put("answer", selectedAnswers.get(i));
+            db.insert("answers", null, values);
         }
 
         db.close();
@@ -191,25 +174,25 @@ public class QuestionnaireFragment extends Fragment {
     private void displaySavedAnswers() {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
         String[] projection = {
-                DatabaseHelper.COLUMN_QUESTION,
-                DatabaseHelper.COLUMN_ANSWER
+                "question",
+                "answer"
         };
 
         Cursor cursor = db.query(
-                DatabaseHelper.TABLE_ANSWERS,   // The table to query
-                projection,                     // The columns to return
-                null,                           // The columns for the WHERE clause
-                null,                           // The values for the WHERE clause
-                null,                           // Don't group the rows
-                null,                           // Don't filter by row groups
-                null                            // The sort order
+                "answers",
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
         );
 
         StringBuilder savedAnswers = new StringBuilder();
         while (cursor.moveToNext()) {
-            String question = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COLUMN_QUESTION));
-            String answer = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseHelper.COLUMN_ANSWER));
-            savedAnswers.append("Question: ").append(question).append("\nAnswer: ").append(answer).append("\n\n");
+            String question = cursor.getString(cursor.getColumnIndexOrThrow("question"));
+            String answer = cursor.getString(cursor.getColumnIndexOrThrow("answer"));
+            savedAnswers.append(question).append(": ").append(answer).append("\n");
         }
         cursor.close();
         db.close();
